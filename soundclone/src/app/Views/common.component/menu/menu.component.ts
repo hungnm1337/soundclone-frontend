523b98bdc5cf1291cfd2c8338a5992c0030442d4
Nguyen Manh Hung <hungnm1337@gmail.com>
2025-07-07 23:40:23 +0700

    change homepage playlist - common

--- soundclone/src/app/Views/common.component/menu/menu.component.ts
+++ soundclone/src/app/Views/common.component/menu/menu.component.ts
@@ -7,5 +7,43 @@ import { Component } from '@angular/core';
   templateUrl: './menu.component.html'
 })
 export class MenuComponent {
+  tracks = [
+    { name: 'Blinding Lights', artist: 'The Weeknd' },
+    { name: 'Dance Monkey', artist: 'Tones and I' },
+    { name: 'Levitating', artist: 'Dua Lipa' },
+    { name: 'Shape of You', artist: 'Ed Sheeran' },
+    { name: 'Bad Guy', artist: 'Billie Eilish' }
+  ];
 
+  artists = [
+    { name: 'Taylor Swift', info: 'Pop • 50M+ listeners' },
+    { name: 'Drake', info: 'Hip-Hop • 45M+ listeners' },
+    { name: 'Ed Sheeran', info: 'Pop • 40M+ listeners' },
+    { name: 'Billie Eilish', info: 'Pop • 35M+ listeners' },
+    { name: 'Ariana Grande', info: 'Pop • 38M+ listeners' }
+  ];
+
+  playlists = [
+    { name: 'Workout Mix', songs: 15 },
+    { name: 'Chill Vibes', songs: 23 },
+    { name: 'Party Hits', songs: 31 },
+    { name: 'Focus Flow', songs: 18 },
+    { name: 'Top 2024', songs: 20 }
+  ];
+
+  showAllTracks = false;
+  showAllArtists = false;
+  showAllPlaylists = false;
+
+  toggleTracks() {
+    this.showAllTracks = !this.showAllTracks;
+  }
+
+  toggleArtists() {
+    this.showAllArtists = !this.showAllArtists;
+  }
+
+  togglePlaylists() {
+    this.showAllPlaylists = !this.showAllPlaylists;
+  }
 }
